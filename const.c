f56254577c2a15c37bc5142723c4587005d8d929
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-09 20:01:27 +0800

    Create const.c

---
+++ const.c
@@ -0,0 +1,11 @@
+#include stdio.h
+int main()
+{
+	const float pi = 3.14;
+	float radius, area;
+	printf(nEnter radius);
+	scanf(%f, &radius);
+	area = piradiusradius;
+	printf(nArea of circle%f, area);
+	return 0;
+}
