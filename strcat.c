c4287354ac622b43f90951cf8afae95c362e751f
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-23 16:00:56 +0800

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char str1[30] = { 0 };
+	char str2[30] = { 0 };
+	printf("输入目的字符串：\n");
+	gets(str1);
+	printf("输入源字符串：\n");
+	gets(str2);
+
+	printf("输出目的字符串：\n");
+	puts(str1);
+	printf("输出源字符串：\n");
+	puts(str2);
+
+	strcat(str1, str2);
+	printf("调用strcpy函数进行字符串复制：\n");
+	printf("字符串连接之后的目的字符串：\n");
+	puts(str1);
+	return 0;
+}
