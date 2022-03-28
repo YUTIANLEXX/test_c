ca7d568c35fb43f610820e19d9579091945a4752
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-28 14:45:01 +0800

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -15,3 +15,61 @@ int main()
 	printf("Êä³öÄ¿±ê×Ö·û´®£º%s\n", ret);
 	return 0;
 }
+
+//Another way to do it
+//#include <stdio.h>
+//void reverse_string(char *arr)
+//{
+//	int left = 0;
+//	int right = strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcd";
+//	reverse_string(arr)	;
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//Another way to do it
+
+//#include <stdio.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char *arr)
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if (my_strlen(arr + 1) >= 2)
+//	{
+//		reverse_string(arr + 1);
+//	}
+//	arr[len - 1] = tmp;
+//}
+//int main()
+//{
+//	char arr[] = "abcd";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
