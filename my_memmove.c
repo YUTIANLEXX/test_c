6e2fb3232afb427e86e4746342a87bd6f1dc73ab
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-05-01 20:28:39 +0800

    Create my_memmove.c

---
+++ my_memmove.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <assert.h>
+
+void* my_memmove(void* dest, const void* src, size_t num)
+{
+	void* ret = dest;
+	assert(dest);
+	assert(src);
+	if (dest < src)
+	{
+		//前->后
+		while (num--)
+		{
+			*(char*)dest = *(char*)src;
+			++(char*)dest;
+			++(char*)src;
+		}
+	}
+	else
+	{
+		//后->前
+		while (num--)
+		{
+			*((char*)dest + num) = *((char*)src + num);
+		}
+	}
+	return ret;
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	my_memmove(arr + 2, arr, 20);
+	return 0;
+}
