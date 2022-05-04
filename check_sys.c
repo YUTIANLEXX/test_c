4e291972f7addd6c8e32a7838cc0eb54a222d662
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-05-04 18:55:03 +0800

    Create check_sys.c

---
+++ check_sys.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int check_sys()
+{
+	int a = 1;
+	return *(char*)&a;
+}
+
+//int check_sys()
+//{
+//	union un
+//	{
+//		char c;
+//		int i;
+//	}u;
+//	u.i = 1;
+//	return u.c;
+//}
+
+int main()
+{
+	int a = 1;
+	int ret = check_sys();
+	if (1 == ret)
+		printf("Ð¡¶Ë\n");
+	else
+		printf("´ó¶Ë\n");
+	return 0;
+}
