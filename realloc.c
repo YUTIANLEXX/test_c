6b6823dbdd865c33e24f834e496aea4255fb16de
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-05-16 18:57:02 +0800

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -34,3 +34,30 @@ int main()
 	ps = NULL;
 	return 0;
 }
+
+//typedef struct S
+//{
+//	int n;
+//	int *arr;
+//};
+//
+//int main()
+//{
+//	int i = 0;
+//	struct S *p = (struct S*)malloc(sizeof(struct S));
+//	p->n = 100;
+//	p->arr = (int *)malloc(10 * sizeof(int));
+//	for (i = 0; i < 10; i++)
+//	{
+//		p->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", p->arr[i]);
+//	}
+//	free(p->arr);
+//	p->arr = NULL;
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
