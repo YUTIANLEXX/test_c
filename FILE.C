432212fff443e9cb94fbb9d08741b304d3a17f02
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-05-22 19:14:05 +0800

    Update FILE.C

--- FILE.C
+++ FILE.C
@@ -9,3 +9,17 @@ int main()
 	pf = NULL;
 	return 0;
 }
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	FILE *pfile;
+//	pfile = fopen("myfile.txt", "w");
+//	if (pfile != NULL)
+//	{
+//		fputs("fopen example", pfile);
+//		fclose(pfile);
+//	}
+//	return 0;
+//}
